1e19dd45bd8f48bdf056d7e9083f0ff8d6012aa3
rene <diaz.de.sandi.7@gmail.com>
2025-06-22 02:59:33 -0700

    feat: add CORS middleware to handle cross-origin requests

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -133,6 +133,24 @@ func init() {
 	)
 }
 
+// CORS
+// TODO: Refactor
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Authorization, Content-Type, X-CSRF-Token")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 
 	l := logger.CreateLogger()
@@ -164,6 +182,7 @@ func main() {
 	cli := humacli.New(func(hooks humacli.Hooks, options *Options) {
 
 		router := chi.NewMux()
+		router.Use(corsMiddleware)
 		api := humachi.New(router, huma.DefaultConfig("Backend - LLM", version))
 		routes.RegisterOllamaRoutes(api)
 
