c8b7616716c5be151a6f07a7d3d38791064f410a
rene <diaz.de.sandi.7@gmail.com>
2025-10-26 23:07:01 -0700

    feat(container): implement application container with model and auth handlers

---
+++ backend/internal/container/container.go
@@ -0,0 +1,43 @@
+package container
+
+import (
+	"github.com/diazdesandi/llm-project/backend/config"
+	"github.com/diazdesandi/llm-project/backend/internal/auth"
+	"github.com/diazdesandi/llm-project/backend/internal/model"
+	supa "github.com/nedpals/supabase-go"
+	"go.uber.org/zap"
+)
+
+type Container struct {
+	ModelHandler *model.Handler
+	AuthHandler  *auth.Handler
+	Logger       *zap.Logger
+}
+
+func NewAppContainer(cfg *config.Config) (*Container, error) {
+
+	// Config
+	l, _ := zap.NewProduction()
+	defaultModel := "tinyllama"
+
+	// Clients
+	modelClient := model.NewClient(cfg.OllamaURL)
+	authClient := supa.CreateClient(cfg.SupabaseURL, cfg.SupabaseKey)
+
+	// Wrap supabase.Client to implement auth.Client interface if needed
+	wrappedAuthClient := auth.NewClient(authClient)
+
+	// Services
+	modelService := model.NewService(modelClient, defaultModel)
+	authService := auth.NewService(*wrappedAuthClient)
+
+	// Handlers
+	modelHandler := model.NewHandler(*modelService)
+	authHandler := &auth.Handler{Service: *authService}
+
+	return &Container{
+		Logger:       l,
+		ModelHandler: modelHandler,
+		AuthHandler:  authHandler,
+	}, nil
+}
