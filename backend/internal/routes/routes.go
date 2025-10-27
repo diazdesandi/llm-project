7451c22b8397280cb5da414f1ab086b1a3ac6ee6
rene <diaz.de.sandi.7@gmail.com>
2025-10-26 23:07:18 -0700

    refactor(routes): migrate from Huma to Gin framework for route handling

--- backend/internal/routes/routes.go
+++ backend/internal/routes/routes.go
@@ -1,81 +1,57 @@
 package routes
 
 import (
-	"context"
 	"fmt"
 
-	"github.com/danielgtaylor/huma/v2"
-	"github.com/diazdesandi/llm-project/backend/internal/handlers"
-	"github.com/diazdesandi/llm-project/backend/internal/metrics"
-	"github.com/prometheus/client_golang/prometheus"
+	"github.com/diazdesandi/llm-project/backend/internal/auth"
+	"github.com/diazdesandi/llm-project/backend/internal/middlewares"
+	"github.com/diazdesandi/llm-project/backend/internal/model"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 type Body struct {
-    Name string `path:"name" maxLength:"30" required:"true" example:"John Doe"`
+	Name string `path:"name" maxLength:"30" required:"true" example:"John Doe"`
 }
 
 type GreetingOutput struct {
-    Body struct {
-        Message string `json:"message" example:"Hello, world!" doc:"Greeting message"`
-    }
+	Body struct {
+		Message string `json:"message" example:"Hello, world!" doc:"Greeting message"`
+	}
 }
 
+func SetupRoutes(mh model.Handler, ah auth.Handler) *gin.Engine {
 
-func RegisterRoutes(app huma.API) {
-    huma.Get(app, "/greeting/{name}", func(ctx context.Context, input *Body) (*GreetingOutput, error) {
-        serviceName := "llm-backend"
-        clientIP := "127.0.0.1"
-        host := "localhost"
+	r := gin.Default()
+	r.Use(gin.Recovery())
+	r.Use(cors.Default())
+	r.Use(middlewares.Metrics())
+	// r.Use(middlewares.CORS())
 
-		metrics.GostServiceRequests.With(prometheus.Labels{
-			"service": serviceName,
-			"client":  clientIP,
-			"host":    host,
-		}).Inc()
+	// Metrics
+	r.GET("/metrics", gin.WrapH(promhttp.Handler()))
 
-		timer := prometheus.NewTimer(metrics.GostServiceRequestDuration.With(prometheus.Labels{
-			"service": serviceName,
-			"client":  clientIP,
-			"host":    host,
-		}))
-		defer timer.ObserveDuration()
+	// Check backend is running, sample endpoint
+	r.GET("/greeting/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		resp := &GreetingOutput{}
+		resp.Body.Message = fmt.Sprintf("Hello, %s!", name)
+		c.JSON(200, resp)
+	})
 
-		inputBytes := 100.0
-		outputBytes := 200.0
+	// model/generate
+	// modelGroup := r.Group("/model")
+	// {
+	// 	modelGroup.GET("/generate", mh.Handler)
+	// }
+	r.POST("/model/generate", mh.Handler)
 
-		metrics.GostServiceInputBytes.With(prometheus.Labels{
-			"service": serviceName,
-			"client":  clientIP,
-			"host":    host,
-		}).Add(inputBytes)
+	authGroup := r.Group("/auth")
+	{
+		authGroup.POST("/signup", ah.SignupHandler)
+		authGroup.POST("/login", ah.SignInHandler)
+	}
 
-		metrics.GostServiceOutputBytes.With(prometheus.Labels{
-			"service": serviceName,
-			"client":  clientIP,
-			"host":    host,
-		}).Add(outputBytes)
-
-		metrics.GostServiceTransferInputBytes.With(prometheus.Labels{
-			"service": serviceName,
-			"client":  clientIP,
-			"host":    host,
-		}).Add(inputBytes)
-
-		metrics.GostServiceTransferOutputBytes.With(prometheus.Labels{
-			"service": serviceName,
-			"client":  clientIP,
-			"host":    host,
-		}).Add(outputBytes)
-
-        resp := &GreetingOutput{}
-        resp.Body.Message = fmt.Sprintf("Hello, %s!", input.Name)
-        return resp, nil
-    })
-
-	huma.Post(app, "/model", handlers.OllamaHandler)
-	
-	// Auth routes
-	// huma.Post(app, "/register", handlers.RegisterHandler)
-	// huma.Post(app, "/login", handlers.LoginHandler)
-	// huma.Get(app, "/profile", handlers.ProfileHandler)
+	return r
 }
